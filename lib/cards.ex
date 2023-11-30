3ee0db881a43686e174e3a286c1cdb89f803ee21
Kevin Gathuku <kevgathuku@gmail.com>
2023-11-30 22:32:47 +0100

    Generate valid deck

--- lib/cards.ex
+++ lib/cards.ex
@@ -3,21 +3,19 @@ defmodule Cards do
   Documentation for `Cards`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def generate_permutations(list1, list2) do
+    Enum.flat_map(list1, fn item1 ->
+      for item2 <- list2, do: {item1, item2}
+    end)
   end
 
   def create_deck do
-    ["Ace", "Two", "Three"]
+    numbers = [:ace, :two, :three, :four, :five, :six, :seven, :eight, :nine, :ten, :king, :queen, :j]
+    suits = [:flowers, :diamonds, :hearts, :spades]
+    # Enum.zip(numbers, suits)
+    Enum.flat_map(numbers, fn number ->
+      for suit <- suits, do: {number, suit}
+    end)
   end
 
   def shuffle(deck) do
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,10 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "generates a valid deck" do
+    deck = Cards.create_deck()
+    assert length(deck) == 52
+    assert Enum.member?(deck, {:two, :hearts}) == true
+    assert Enum.member?(deck, {:three, :diamonds}) == true
   end
 end
