d53de3882a41f08b958483ddbafa20140f15c52d
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-01-24 21:04:19 +0100

    Functioning server tests

--- lib/games/kadi/server.ex
+++ lib/games/kadi/server.ex
@@ -1,6 +1,6 @@
 defmodule Games.Kadi.Server do
   @moduledoc """
-  Card dealer module
+  Kadi Game Server
   """
   require Logger
 
@@ -15,7 +15,40 @@ defmodule Games.Kadi.Server do
     played: []
   }
 
-  ## Client API
+  def init(options \\ %{}) do
+    # Merge default and provided config options
+    final_options = Map.merge(@default_config, Enum.into(options, %{}))
+
+    deck = Utils.create_deck() |> Enum.shuffle()
+
+    state =
+      @default_state
+      |> Map.put(:deck, deck)
+      |> Map.put(:options, final_options)
+
+    {:ok, state}
+  end
+
+  @doc """
+  Add a player associated with the given `name` in `server`.
+  """
+  def add_player(%{players: players} = state, name) do
+    case Enum.any?(players, fn player -> player.name == name end) do
+      false ->
+        # Player does not exist
+        Logger.info("Adding Player: #{name}")
+        player = %Games.Kadi.Player{name: name, cards: []}
+
+        new_state = %{state | players: [player | players]}
+
+        {:ok, new_state}
+
+      _ ->
+        Logger.info("Player #{name} already exists")
+        {:ok, state}
+    end
+  end
+
   @doc """
   Starts the game.
   """
@@ -23,8 +56,10 @@ defmodule Games.Kadi.Server do
     # TODO: Do any initial setup here
     # Deal x cards to the players
     # Play the starting card
-    Enum.reduce(players, state, fn x, acc -> deal(acc, x, 4) end)
+    state = Enum.reduce(players, state, fn x, acc -> deal(acc, x, 4) end)
     |> assign_start_card()
+
+    {:ok, state}
   end
 
   @doc """
@@ -43,45 +78,12 @@ defmodule Games.Kadi.Server do
     # GenServer.call(server, :shuffle)
   end
 
-  @doc """
-  Add a player associated with the given `name` in `server`.
-  """
-  def add_player(%{players: players} = state, name) do
-    case Enum.any?(players, fn player -> player.name == name end) do
-      false ->
-        # Player does not exist
-        player = %Games.Kadi.Player{name: name, cards: []}
-
-        new_state = %{state | players: [player | players]}
-
-        {:ok, new_state}
-
-      _ ->
-        Logger.info("Player #{name} already exists")
-        {:ok, state}
-    end
-  end
-
   def handle_hand(%{players: players} = state, player, cards) do
     player = Enum.find(players, fn x -> x.name == player end)
     # Is it the player's turn?
     # Is the hand valid?
     is_valid = is_valid_hand?(cards)
     # Compute the next state based on the new hand
-  end
-
-  def init(options) do
-    # Merge default and provided config options
-    final_options = Map.merge(@default_config, Enum.into(options, %{}))
-
-    deck = Utils.create_deck() |> Enum.shuffle()
-
-    state =
-      @default_state
-      |> Map.put(:deck, deck)
-      |> Map.put(:options, final_options)
-
-    {:ok, state}
   end
 
   @doc """
--- test/games/kadi/server_test.exs
+++ test/games/kadi/server_test.exs
@@ -2,58 +2,34 @@ defmodule Games.Kadi.ServerTest do
   use ExUnit.Case, async: true
   alias Games.Kadi.Server
 
-  setup context do
-    # Read the :num_players tag value
-    case context do
-      %{num_players: num_players} ->
-        %{state: %{num_players: num_players}}
+  # setup context do
+  #   # Read the :num_players tag value
+  #   case context do
+  #     %{num_players: num_players} ->
+  #       %{config: %{num_players: num_players}}
 
-      _ ->
-        # registry = start_supervised!(Dealer)
-        %{registry: registry}
-    end
-  end
+  #     _ ->
+  #       # registry = start_supervised!(Dealer)
+  #       %{config: %{}}
+  #   end
+  # end
 
-  test "generates valid deck on init", %{registry: registry} do
-    %{deck: deck} = :sys.get_state(registry)
-
+  test "generates valid deck on init with no options" do
     suits = ~w(Hearts Flowers Diamonds Spades)
+    {:ok, %{deck: deck}} = Server.init()
 
     assert length(deck) == 52
     assert Enum.all?(deck, fn {_, suit} -> Enum.member?(suits, suit) end)
   end
 
-  test "deals 4 cards to each player on start game", %{registry: registry} do
-    Dealer.add_player(registry, "Kevin")
-    Dealer.add_player(registry, "King")
-    Dealer.start_game(registry)
-
-    %{players: players, deck: deck} = :sys.get_state(registry)
-
-    assert length(players) == 2
-    assert Enum.all?(players, fn player -> length(player.cards) == 4 end) == true
-    assert length(deck) == 44
-  end
-
-  test "assigns correct first card on start" do
-    Dealer.add_player("Kevin")
-    Dealer.add_player("King")
-    %{played: played} = Dealer.start_game()
-
-    {num, _} = hd(played)
-
-    assert length(played) == 1
-    refute num in [?A, ?K, ?J, ?Q, 2, 3, 8]
-  end
-
-  test "add player by name", %{registry: registry} do
+  test "add player by name" do
     name = "iniesta"
 
-    %{deck: deck, players: players} = Dealer.add_player(registry, name)
+    {:ok, init_state} = Server.init()
+    {:ok, %{deck: deck, players: players}} = Server.add_player(init_state, name)
     player = Enum.find(players, fn player -> player.name == name end)
 
     assert player in players
-    assert player.name == name
     # No cards assigned at this point yet
     assert length(player.cards) == 0
 
@@ -61,34 +37,62 @@ defmodule Games.Kadi.ServerTest do
     assert length(deck) == 52
   end
 
-  test "does not add duplicate players", %{registry: registry} do
+  test "does not add duplicate players" do
     name = "iniesta"
 
-    Dealer.add_player(registry, name)
-    Dealer.add_player(registry, name)
+    {:ok, init_state} = Server.init()
+    {:ok, with_player_1} = Server.add_player(init_state, name)
+    {:ok, %{players: players, deck: deck}} = Server.add_player(with_player_1, name)
 
-    %{deck: deck, players: players} = :sys.get_state(registry)
+    # %{deck: deck, players: players} = :sys.get_state(registry)
     assert length(players) == 1
     assert length(deck) == 52
   end
 
-  test "accepts a play from the next player", %{registry: registry} do
-    Dealer.add_player(registry, "Boo")
-    Dealer.add_player(registry, "Doo")
+  test "assigns correct first card on start game" do
+    {:ok, state} = Server.init()
+    {:ok, with_player_1} = Server.add_player(state, "Kevin")
+    {:ok, with_player_2} = Server.add_player(with_player_1, "King")
+    {:ok, %{played: played}} = Server.start_game(with_player_2)
 
-    # TODO: Figure out a way to mock the cards to assign
-    Dealer.play_hand(registry, "Boo", [])
+    {num, _} = hd(played)
+
+    assert length(played) == 1
+    refute num in [?A, ?K, ?J, ?Q, 2, 3, 8]
   end
 
-  test "does not accept a play from other players", %{registry: registry} do
-    Dealer.add_player(registry, "Boo")
-    Dealer.add_player(registry, "Doo")
+  test "deals 4 cards to each player on start game" do
+    {:ok, state} = Server.init()
+    {:ok, with_player_1} = Server.add_player(state, "Kevin")
+    {:ok, with_player_2} = Server.add_player(with_player_1, "King")
+    # {:ok, final_state} = Server.start_game(with_player_2)
+    {:ok, %{players: players, deck: deck}} = Server.start_game(with_player_2)
 
-    state_before = :sys.get_state(registry)
+    # %{players: players, deck: deck} = final_state
 
-    # TODO: Figure out a way to mock the cards to assign
-    Dealer.play_hand(registry, "Doo", [])
-    state_after = :sys.get_state(registry)
-    assert state_before == state_after
+    assert length(players) == 2
+    assert Enum.all?(players, fn player -> length(player.cards) == 4 end) == true
+    assert length(deck) == 44
   end
+
+  # test "accepts a play from the next player", %{registry: registry} do
+  #   {:ok, state} = Server.init()
+  #   {:ok, with_player_1} = Server.add_player(state, "Boo")
+  #   {:ok, with_player_2} = Server.add_player(with_player_1, "Doo")
+
+  #   # TODO: Figure out a way to mock the cards to assign
+  #   Server.play_hand(registry, "Boo", [])
+  # end
+
+  # test "does not accept a play from other players", %{registry: registry} do
+  #   Server.add_player(registry, "Boo")
+  #   Server.add_player(registry, "Doo")
+
+  #   state_before = :sys.get_state(registry)
+
+  #   # TODO: Figure out a way to mock the cards to assign
+  #   Server.play_hand(registry, "Doo", [])
+  #   state_after = :sys.get_state(registry)
+  #   assert state_before == state_after
+  # end
 end
