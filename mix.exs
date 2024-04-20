20d519f5e03bd55980081cb903565ca8548a9883
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-04-20 23:06:01 +0200

    Elixir 1.16

--- mix.exs
+++ mix.exs
@@ -5,10 +5,10 @@ defmodule Cards.MixProject do
     [
       app: :cards,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      compilers: [:finitomata] ++ Mix.compilers,
+      compilers: [:finitomata | Mix.compilers()],
     ]
   end
 
