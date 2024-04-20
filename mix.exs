8ae45c2407d58f7f2ef2a67cdde842e3537b447d
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-04-20 12:45:08 +0200

    Add finitomata to compilers

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Cards.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      compilers: [:finitomata] ++ Mix.compilers,
     ]
   end
 
