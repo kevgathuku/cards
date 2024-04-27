176db4c868d35b40e4f03d8b1f31776ac547a83a
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-04-27 12:27:30 +0200

    Verify that it does not allow duplicate players

--- test/games/kadi/fsm_server_test.exs
+++ test/games/kadi/fsm_server_test.exs
@@ -44,7 +44,7 @@ defmodule Games.Kadi.FSMServerTest do
     test "start with bogus config", ctx do
       assert_transition ctx, {:start, %{obviously_this_is_invalid: 67}} do
         :lobby ->
-          # TODO: Figure out a way to test the map keys
+          # TODO: Find out how to test exclusion of a map key
           assert_payload do
             rules.obviously_this_is_invalid ~> true
           end
@@ -70,6 +70,29 @@ defmodule Games.Kadi.FSMServerTest do
         assert_state :awaiting_deck do
           assert_payload(%{
             players: [%Player{name: "Kevin", cards: []}, %Player{name: "Devin", cards: []}]
+          })
+        end
+    end
+
+    test_path "adding duplicate players", _ctx do
+      {:start, %{min_players: 2}} ->
+        assert_state :lobby do
+          assert_payload(%{
+            rules: %{min_players: 2}
+          })
+        end
+
+      {:add_player, "Kevin"} ->
+        assert_state :lobby do
+          assert_payload(%{
+            players: [%Player{name: "Kevin", cards: []}]
+          })
+        end
+
+      {:add_player, "Kevin"} ->
+        assert_state :lobby do
+          assert_payload(%{
+            players: [%Player{name: "Kevin", cards: []}]
           })
         end
     end
