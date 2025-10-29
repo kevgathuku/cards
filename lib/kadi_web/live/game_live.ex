37c7657263935cba0068c71910420795f6dae98d
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2025-10-29 23:06:41 +0100

    Add start game button

--- lib/kadi_web/live/game_live.ex
+++ lib/kadi_web/live/game_live.ex
@@ -23,4 +23,20 @@ defmodule KadiWeb.GameLive do
          |> redirect(to: ~p"/lobby")}
     end
   end
+
+  @impl true
+  def handle_event("start_game", _, socket) do
+    game_session = socket.assigns.game_session
+
+    case CardGames.start_game(game_session) do
+      {:ok, updated_game_session} ->
+        {:noreply,
+         socket
+         |> assign(:game_session, updated_game_session)
+         |> put_flash(:info, "Game started!")}
+
+      {:error, :not_enough_players} ->
+        {:noreply, put_flash(socket, :error, "Not enough players to start the game.")}
+    end
+  end
 end
