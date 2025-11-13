a157e60bd644bc49c7b719dfda85d28bb2523b0e
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2025-11-13 21:13:29 +0100

    Avoid dealing 8 as start card

--- lib/kadi/card_games.ex
+++ lib/kadi/card_games.ex
@@ -1070,8 +1070,8 @@ defmodule Kadi.CardGames do
   defp select_start_card(deck_cards) do
     # Per FR-003: Kings are allowed as start cards (no direction reversal occurs)
     # Aces are also allowed as start cards (player can play any suit, no suit selection required)
-    # Other special cards (2, 3, jack, queen) are excluded
-    special_ranks = ["2", "3", "jack", "queen"]
+    # Other special cards (2, 3, 8, jack, queen) are excluded
+    special_ranks = ["2", "3", "8", "jack", "queen"]
     shuffled_cards = Enum.shuffle(deck_cards)
 
     start_card =
