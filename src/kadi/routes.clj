a1b58875ae49b2fca946e61b638afac1465bad24
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2026-03-14 18:30:43 +0100

    Use var references in routes for REPL hot-reload support
    Handler references now use #'h/handler instead of h/handler so that
    reloading namespaces in the REPL takes effect without restarting the server.

--- src/kadi/routes.clj
+++ src/kadi/routes.clj
@@ -1,34 +1,36 @@
 (ns kadi.routes
-  "HTTP routes."
+  "HTTP routes.
+   All handlers are referenced via vars (#') so that reloading handler
+   namespaces in the REPL takes effect without restarting the server."
   (:require [kadi.handlers :as h]))
 
 (def routes
   [;; Home
-   ["/" {:get h/index}]
+   ["/" {:get #'h/index}]
 
    ;; Auth
    ["/auth"
-    ["/signin" {:get h/signin-page}]
-    ["/send-link" {:post h/send-signin-link}]
-    ["/verify/:token" {:get h/verify-signin}]
-    ["/signout" {:post h/signout}]]
+    ["/signin" {:get #'h/signin-page}]
+    ["/send-link" {:post #'h/send-signin-link}]
+    ["/verify/:token" {:get #'h/verify-signin}]
+    ["/signout" {:post #'h/signout}]]
 
    ;; Join game (separate to avoid conflict with /:code)
-   ["/join" {:get (h/require-auth h/join-page)
-             :post (h/require-auth h/join-game-by-code)}]
+   ["/join" {:get (h/require-auth #'h/join-page)
+             :post (h/require-auth #'h/join-game-by-code)}]
 
    ;; Games
    ["/games"
-    ["" {:get h/list-games
-         :post h/create-game}]
-    ["/:code" {:get (h/require-auth h/get-game)}]
-    ["/:code/join" {:post (h/require-auth h/join-game)}]
-    ["/:code/start" {:post (h/require-auth h/start-game)}]
-    ["/:code/play" {:post (h/require-auth h/play-cards)}]
-    ["/:code/draw" {:post (h/require-auth h/draw-card)}]
-    ["/:code/select-suit" {:post (h/require-auth h/select-suit)}]
-    ["/:code/accept-penalty" {:post (h/require-auth h/accept-penalty)}]
-    ["/:code/answer-question" {:post (h/require-auth h/answer-question)}]
-    ["/:code/players" {:get (h/require-auth h/get-players-fragment)}]
-    ["/:code/lobby-status" {:get (h/require-auth h/get-lobby-status-fragment)}]
-    ["/:code/state" {:get (h/require-auth h/get-game-state-fragment)}]]])
+    ["" {:get #'h/list-games
+         :post #'h/create-game}]
+    ["/:code" {:get (h/require-auth #'h/get-game)}]
+    ["/:code/join" {:post (h/require-auth #'h/join-game)}]
+    ["/:code/start" {:post (h/require-auth #'h/start-game)}]
+    ["/:code/play" {:post (h/require-auth #'h/play-cards)}]
+    ["/:code/draw" {:post (h/require-auth #'h/draw-card)}]
+    ["/:code/select-suit" {:post (h/require-auth #'h/select-suit)}]
+    ["/:code/accept-penalty" {:post (h/require-auth #'h/accept-penalty)}]
+    ["/:code/answer-question" {:post (h/require-auth #'h/answer-question)}]
+    ["/:code/players" {:get (h/require-auth #'h/get-players-fragment)}]
+    ["/:code/lobby-status" {:get (h/require-auth #'h/get-lobby-status-fragment)}]
+    ["/:code/state" {:get (h/require-auth #'h/get-game-state-fragment)}]]])
